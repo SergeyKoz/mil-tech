a3607c929aef40e992013c0b229fee3305f5c44f
Serhii Kozin <s.kozin@evernode.tech>
2026-06-11 21:10:02 +0000

    homework06 rework - added all functions HW 1 - performed errors - added tests

--- homework_06/include/ballistics.hpp
+++ homework_06/include/ballistics.hpp
@@ -11,15 +11,28 @@ struct AmmoParams {
     float lift;  // коефіцієнт підйому
 };
 
+struct Coord {
+    float x;
+    float y;
+};
+
 const AmmoParams ammoList[AMMO_COUNT] = {{.name = "VOG-17", .mass = 0.35F, .drag = 0.07F, .lift = 0.0F},
                                          {.name = "M67", .mass = 0.60F, .drag = 0.10F, .lift = 0.0F},
                                          {.name = "RKG-3", .mass = 1.20F, .drag = 0.10F, .lift = 0.0F},
                                          {.name = "GLIDING-VOG", .mass = 0.45F, .drag = 0.10F, .lift = 1.0F},
                                          {.name = "GLIDING-RKG", .mass = 1.40F, .drag = 0.10F, .lift = 1.0F}};
 
-struct DropParameters {
+struct BallisticParameters {
     float time;      // час падіння
     float distance;  // відстань, на яку відхилиться снаряд від вертикалі
 };
 
-auto calc_drop_parameters(const char ammo_name[32], float v0, float z0) -> DropParameters;
+struct FireParameters {
+    float distance;      // відстань до цілі
+    Coord fire;          // координати вогню
+    Coord intermediate;  // координати проміжної точки для корекції
+};
+
+auto calc_ballistic_parameters(const char ammo_name[32], float v0, float z0) -> BallisticParameters;
+
+auto calc_fire_parameters(BallisticParameters ballistic_params, Coord drone, Coord target, float accelerationPath) -> FireParameters;
