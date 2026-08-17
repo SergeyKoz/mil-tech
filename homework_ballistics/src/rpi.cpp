9908b4d1804f573abd55850f2c8237da0d1b52c3
Serhii Kozin <sergagame1@gmail.com>
2026-08-17 19:25:28 +0300

    - rpi checker update

--- homework_ballistics/src/rpi.cpp
+++ homework_ballistics/src/rpi.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include "DroneAutopilot.hpp"
-#include "MissionFactory.hpp"
+#include "RpiMissionFactory.hpp"
 #include "rpiChecker/RpiCheckerGPIO.hpp"
 #include "rpiChecker/RpiCheckerUART.hpp"
 #include "CliParams.hpp"
 
 auto main(int argc, char *argv[]) -> int
 {
-    try {
+    try
+    {
         auto cliParams = CliParams::parse(argc, argv);
 
-        auto configLoader = MissionFactory::createConfigLoader(ConfigLoaderType::CHECKER, AmmoLoaderType::NONE, nullptr);
-        auto targetProvider = MissionFactory::createTargetsProvider(ProviderType::CHECKER);
-        auto ballisticsSolver = MissionFactory::createBallisticsSolver(SolverType::TABLE);
+        auto configLoader = RpiMissionFactory::createConfigLoader(ConfigLoaderType::CHECKER, AmmoLoaderType::NONE);
+        auto targetProvider = RpiMissionFactory::createTargetsProvider(ProviderType::CHECKER);
+        auto ballisticsSolver = RpiMissionFactory::createBallisticsSolver(SolverType::TABLE);
         auto rpiCheckerGPIO = std::make_unique<RpiCheckerGPIO>(cliParams.gpioChip, cliParams.startLine, cliParams.dropLine);
         auto rpiCheckerUART = std::make_unique<RpiCheckerUART>(cliParams.uartPort);
 
@@ -28,17 +29,20 @@ auto main(int argc, char *argv[]) -> int
 
         LOG("Mission completed successfully.");
     }
-    catch (const std::runtime_error &ex) {
+    catch (const std::runtime_error &ex)
+    {
         std::cerr << ex.what() << '\n';
 
         return 1;
     }
-    catch (const std::invalid_argument &ex) {
+    catch (const std::invalid_argument &ex)
+    {
         std::cerr << ex.what() << '\n';
 
         return 1;
     }
-    catch (const std::exception &ex) {
+    catch (const std::exception &ex)
+    {
         std::cerr << ex.what() << '\n';
 
         return 1;
